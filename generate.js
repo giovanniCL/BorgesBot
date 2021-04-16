3940f8fb648b2f51e125533c7f6d325704473a2e
unknown <giovanniciccia1@gmail.com>
2021-04-16 00:42:34 -0400

    cleaned commented out code

--- generate.js
+++ generate.js
@@ -4,7 +4,6 @@ const model = JSON.parse(fs.readFileSync('model.json', 'latin1'))
 function getWord(words, probability){
     if(words.length == 1) return words[0][0]
     let possible_words = []
-    //console.log(words)
     for(let i = (words.length) - 1; i > 0; i--){
         let word = words[i][0]
         if(probability > words[i-1][1]) {
@@ -13,8 +12,6 @@ function getWord(words, probability){
         }
     }
     let rand = Math.floor(Math.random() * possible_words.length)
-    //console.log(rand)
-    //console.log(possible_words[rand])
     if(possible_words.length == 0) return words[0][0]
     return possible_words[rand]
 }
