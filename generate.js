5923c64bf60c43f58500bb556ccf9d9b01605b2f
unknown <giovanniciccia1@gmail.com>
2021-04-19 23:16:15 -0400

    experimented with pos tagger

--- generate.js
+++ generate.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const model = JSON.parse(fs.readFileSync('model.json', 'latin1'))
 
 function getWord(words, probability){
+    
     if(words.length == 1) return words[0][0]
     let possible_words = []
     for(let i = (words.length) - 1; i > 0; i--){
@@ -14,19 +15,65 @@ function getWord(words, probability){
     let rand = Math.floor(Math.random() * possible_words.length)
     if(possible_words.length == 0) return words[0][0]
     return possible_words[rand]
+    
 }
 
-function generate(model){
-    sentence = ""
-    nextWord = getWord(model["<START>"],Math.random())
+function word_generate(model){
+    let bigram_prob = model.bigram_prob
+    let sentence = ""
+    nextWord = getWord(bigram_prob["<START>"],Math.random())
     while(nextWord != "<END>"){
         let space = (nextWord == "." || nextWord == ",") ? "" : " "
         sentence += space + nextWord
-        nextWord = getWord(model[nextWord],Math.random())
+        nextWord = getWord(bigram_prob[nextWord],Math.random())
 
     }
     return sentence.trim()
 }
 
-let output = generate(model)
+function pos_generate(model){
+    let bigram_pos_prob = model.bigram_pos_prob
+    let pos_prob = model.pos_prob
+    pos_list = []
+    nextPOS = getWord(bigram_pos_prob["<START>"], Math.random())
+    while(nextPOS != "<END>"){
+        pos_list.push(nextPOS)
+        nextPOS = getWord(bigram_pos_prob[nextPOS], Math.random())
+    }
+    let sentence = ""
+    pos_list.forEach(pos=>{
+        let nextWord = getWord(pos_prob[pos], Math.random())
+        let space = (nextWord == "." || nextWord == ",") ? "" : " "
+        sentence += space + nextWord
+    })
+    return sentence.trim()
+}
+
+function hybrid_generate(model){
+    let bigram_prob = model.bigram_prob
+    let bigram_pos_prob = model.bigram_pos_prob
+    let pos_prob = model.pos_prob
+    pos_list = []
+    nextPOS = getWord(bigram_pos_prob["<START>"], Math.random())
+    while(nextPOS != "<END>"){
+        pos_list.push(nextPOS)
+        nextPOS = getWord(bigram_pos_prob[nextPOS], Math.random())
+    }
+    let sentence = ""
+    let currentWord = "<START>"
+    pos_list.forEach(pos=>{
+        
+        let nextWord = getWord(pos_prob[pos], Math.random())
+        while(bigram_prob[currentWord].map(word=>word[0]).includes(nextWord)){
+           nextWord = getWord(pos_prob[pos], Math.random())
+
+        }
+        currentWord = nextWord
+        let space = (nextWord == "." || nextWord == ",") ? "" : " "
+        sentence += space + nextWord
+    })
+    return sentence.trim()
+}
+
+let output = word_generate(model)
 console.log(output)
--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 from nltk import word_tokenize
+import stanza
+nlp = stanza.Pipeline(lang = "es", processors='tokenize,pos')
 import json
 
 in_file = open("corpus.txt", 'r', encoding = "utf-8")
 corpus = in_file.read()
+
 sentences = []
 new_sent = ""
 for char in corpus:
@@ -12,32 +15,19 @@ for char in corpus:
         new_sent = ""
 
 tokens_list = []
+pos_list = []
 for sentence in sentences:
-    tokens = word_tokenize(sentence)
-    tokens_list.append(tokens)
+    doc = nlp(sentence)
+    pos_list.append([word.xpos for sent in doc.sentences for word in sent.words])
+    tokens_list.append([word.text for sent in doc.sentences for word in sent.words])
 
-unigram_freq = {}
-unigram_prob = {}
-word_sum = 0
-for tokens in tokens_list:
-    for token in tokens:
-        word_sum += 1
-        if token in unigram_freq:
-            unigram_freq[token] += 1
-        else:
-            unigram_freq[token] = 1
+def addToDict(key, dict):
+    if key in dict:
+        dict[key] += 1
+    else:
+        dict[key] = 1
 
-for tokens in tokens_list:
-    for token in tokens:
-        freq = unigram_freq[token]
-        unigram_prob[token] = freq/word_sum
-
-unigram_freq["<START>"] = len(sentences)
-unigram_freq["<END>"] = len(sentences)
-
-bigram_freq = {}
-bigram_prob = {}
-def addToDict(key, value, dict):
+def addToDictofDicts(key, value, dict):
     if key in dict:
         entry =dict[key]
         if value not in entry:
@@ -46,21 +36,56 @@ def addToDict(key, value, dict):
     else:
         dict[key] = {value:1}
 
-for tokens in tokens_list:
-    addToDict("<START>", tokens[0], bigram_freq )
-    for i in range(len(tokens)-1):
-        addToDict(tokens[i],tokens[i+1],bigram_freq)
-    addToDict(tokens[len(tokens)-1],"<END>",bigram_freq)
+def probabilize(bigram_dict, unigram_dict):
+    new_dict = {}
+    for key, value in bigram_dict.items():
+        denominator = unigram_dict[key]
+        for word,freq in value.items():
+            probability = freq/denominator
+            if key in new_dict:
+                new_dict[key].append([word,probability])
+            else:
+                new_dict[key] = []
+                new_dict[key].append([word,probability])
+    return new_dict
 
-for key, value in bigram_freq.items():
-    denominator = unigram_freq[key]
-    for word,freq in value.items():
-        probability = freq/denominator
-        if key in bigram_prob:
-            bigram_prob[key].append([word,probability])
-        else:
-            bigram_prob[key] = []
-            bigram_prob[key].append([word,probability])
+unigram_freq = {}
+unigram_prob = {}
+unigram_pos_freq = {}
+bigram_freq = {}
+bigram_prob = {}
+bigram_pos_freq = {}
+word_sum = 0
+unigram_freq["<START>"] = len(sentences)
+unigram_freq["<END>"] = len(sentences)
+unigram_pos_freq["<START>"] = len(sentences)
+unigram_pos_freq["<END>"] = len(sentences)
+pos_dict = {}
+for i in range(len(tokens_list)):
+    addToDictofDicts("<START>", tokens_list[i][0], bigram_freq)
+    addToDictofDicts("<START>", pos_list[i][0], bigram_pos_freq)
+    for j in range(len(tokens_list[i]) - 1):
+        word_sum +=1
+        token = tokens_list[i][j]
+        pos = pos_list[i][j]
+        addToDict(token, unigram_freq)
+        addToDict(pos, unigram_pos_freq)
+        addToDictofDicts(token, tokens_list[i][j + 1], bigram_freq)
+        addToDictofDicts(pos, pos_list[i][j + 1], bigram_pos_freq)
+        addToDictofDicts(pos, token, pos_dict)
+    addToDict(tokens_list[i][len(tokens_list[i]) - 1], unigram_freq)
+    addToDict(pos_list[i][len(pos_list[i]) - 1], unigram_pos_freq)
+    addToDictofDicts(tokens_list[i][len(tokens_list[i]) - 1], "<END>", bigram_freq)
+    addToDictofDicts(pos_list[i][len(pos_list[i]) - 1], "<END>", bigram_pos_freq)
+    addToDictofDicts(pos_list[i][len(pos_list[i])-1], tokens_list[i][len(tokens_list[i]) - 1], pos_dict)
+
+bigram_prob = probabilize(bigram_freq, unigram_freq)
+bigram_pos_prob = probabilize(bigram_pos_freq,unigram_pos_freq)
+pos_prob = probabilize(pos_dict, unigram_pos_freq)
+
+print(bigram_pos_prob["<START>"])
+
+
 
 def sortFunc(x):
     return x[1]
@@ -68,7 +93,15 @@ def sortFunc(x):
 for key, value in bigram_prob.items():
     value.sort(key = sortFunc)
 
-j = json.dumps(bigram_prob, ensure_ascii=False)
+for key, value in bigram_pos_prob.items():
+    value.sort(key = sortFunc)
+model = {
+    "bigram_prob" : bigram_prob,
+    "bigram_pos_prob" : bigram_pos_prob,
+    "pos_prob" : pos_prob
+}
+
+j = json.dumps(model, ensure_ascii=False)
 with open("model.json",'w') as f:
     f.write(j)
     f.close()
